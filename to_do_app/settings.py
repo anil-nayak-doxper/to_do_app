7d01e7e927b25c371b13d1a92156df2c15941c43
anilnayak3655 <nayak.anil3655@gmail.com>
2022-03-15 23:06:48 +0530

    changes for heroku

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u)(hyyeh9t87i%bioa*o%g(#!bp=&0#ny9xmi*ubp%dw^1f(%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
