c6b585553fe87616542d3e0cf9849cae262395cc
anilnayak3655 <nayak.anil3655@gmail.com>
2022-03-15 19:36:28 +0530

    some changes for heroku setup

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,6 +1,5 @@
 
 from datetime import datetime, timedelta
-from turtle import title
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework import status
